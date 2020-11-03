21dde15d682e9f4c3c7877d55b07a68cedf19a9d
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-11-03 17:51:24 -0300

    código inicial

--- main.c
+++ main.c
@@ -5,7 +5,37 @@
 
 // gcc main.c -o main -lpthread
 
-int main() {
-	printf("TODO");
+typedef struct {
+	pthread_mutex_t m;
+
+	//TODO
+} Input;
+
+
+void* philosophize(void* args) {
+	//TODO
+}
+
+int main(int argc, char *argv) {
+
+	if (argc != 3) {
+		printf("Wrong args");
+		exit(1);
+	}
+
+	int n = argv[1]; //filósofos
+	int m = argv[2]; //macarrão
+
+	pthread_t* vphil = (pthread_t*)malloc(n * sizeof(pthread_t));;
+
+	Input dta;
+	pthread_mutex_init(&dta.m, NULL);
+
+	for (int i = 0; i < n; i++)
+		pthread_create(&(vphil[i]), NULL, philosophize, (void*)&dta);
+
+	for (int i = 0; i < n; i++)
+		pthread_join(vphil[i], (void*)&dta);
+
 	return 0;
 }
