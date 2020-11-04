84994ce0a81024749ed4bbeb2a9ba7aeec7dd38c
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-11-04 17:09:02 -0300

    não faço ideia do q q eu to fazendo

--- main.c
+++ main.c
@@ -2,18 +2,42 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
 
 // gcc main.c -o main -lpthread
 
 typedef struct {
 	pthread_mutex_t m;
+	sem_t pgarfos;
 
-	//TODO
+	int macarrao;
+	int fim;
 } Input;
 
 
 void* philosophize(void* args) {
-	Input* dta = (Input*)arg;
+	Input* dta = (Input*)args;
+
+	while (!(dta->fim)) {
+		sleep(dta->n);
+
+		sem_wait(&(dta->pgarfos));
+		sem_wait(&(dta->pgarfos));
+		pthread_mutex_lock(&(dta->m));
+
+		dta->macarrao--;
+
+		if (dta->macarrao == 0) {
+		    dta->fim = 1;
+		    sem_post(&(dta->pgarfos));
+		    sem_post(&(dta->pgarfos));
+		}
+
+		pthread_mutex_unlock(&(dta->m));
+		sem_post(&(dta->pgarfos));
+		sem_post(&(dta->pgarfos));
+	}
 }
 
 int main(int argc, char *argv) {
@@ -23,12 +47,16 @@ int main(int argc, char *argv) {
 		exit(1);
 	}
 
-	int n = argv[1]; //filósofos
-	int m = argv[2]; //macarrão
+	//int n = argv[1];
+	//int n = 2;
 
 	pthread_t* vphil = (pthread_t*)malloc(n * sizeof(pthread_t));;
 
 	Input dta;
+	//dta.macarrao = argv[2];
+	//dta.macarrao = 10;
+    dta.fim = 0;
+	sem_init(&dta.pgarfos, 0, n);
 	pthread_mutex_init(&dta.m, NULL);
 
 	for (int i = 0; i < n; i++)
